bd549c4863a89835a778845d3e01ff42567efdc9
shawngenese <shawngenese25@gmail.com>
2026-03-31 19:12:56 +0800

    artists and albums

--- js/showArtist.js
+++ js/showArtist.js
@@ -1,3 +1,5 @@
+
+
 const popularArtist = {
     'IV of Spades': [
         { number: 1, image: '../assets/images/4ofSpades/music-cover-04.jpg', title: "Mundo", time: "5:49", listeners: "360,151,156", album: "Single", artistProfile: "../assets/images/4ofSpades/profile.jpg", artistCover: "../assets/images/4ofSpades/cover.jpg", monthlyListeners: "7,341,027 Monthly Listeners"},
@@ -5,7 +7,70 @@ const popularArtist = {
         { number: 3, image: '../assets/images/4ofSpades/music-cover-02.jpg', title: "Come Inside Of My Heart", time: "4:07", listeners: "337,388,294", album: "CLAPCLAPCLAP!"},
         { number: 4, image: '../assets/images/4ofSpades/music-cover-02.jpg', title: "Dulo Ng Hangganan", time: "5:29", listeners: "126,691,312", album: "CLAPCLAPCLAP!"},
         { number: 5, image: '../assets/images/4ofSpades/music-cover-03.jpg', title: "Kabisado", time: "3:27", listeners: "6,399,411", album: "Andalucia"},
-    ]
+    ],
+    'Arthur Nery': [
+        { number: 1, image: '../assets/images/arthurNery/music-cover-01.png', title: "Isa Lang", time: "4:29", listeners: "485,004,142", album: "Single", artistProfile: "../assets/images/arthurNery/profile.jpg", artistCover: "../assets/images/arthurNery/cover.jpg", monthlyListeners: "9,157,353 Monthly Listeners"},
+        { number: 2, image: '../assets/images/arthurNery/music-cover-03.png', title: "Palaisipan", time: "4:01", listeners: "91,531,123", album: "Single" },
+        { number: 3, image: '../assets/images/arthurNery/music-cover-05.png', title: "TAKE ALL THE LOVE", time: "3:26", listeners: "73,364,122", album: "Single"},
+        { number: 4, image: '../assets/images/arthurNery/music-cover-04.png', title: "Palayo Sa Mundo", time: "4:28", listeners: "29,492,531", album: "Single"},
+        { number: 5, image: '../assets/images/arthurNery/music-cover-02.png', title: "Pagsamo", time: "4:57", listeners: "421,83,412", album: "Single"},
+    ],
+    'Earl Agustin': [
+        { number: 1, image: '../assets/images/earlAgustin/music-cover-01.png', title: "Tibok", time: "5:26", listeners: "34,541,451", album: "Himig At Pag-ibig", artistProfile: "../assets/images/earlAgustin/profile.jpg", artistCover: "../assets/images/earlAgustin/profile.jpg", monthlyListeners: "5,980,739 Monthly Listeners"},
+        { number: 2, image: '../assets/images/earlAgustin/music-cover-01.png', title: "Panalangin", time: "4:12", listeners: "64,125,436", album: "Himig At Pag-ibig" },
+        { number: 3, image: '../assets/images/earlAgustin/music-cover-01.png', title: "Pag-Ibig ng Ikaw at Ako", time: "4:40", listeners: "345,234,542", album: "Himig At Pag-ibig"},
+        { number: 4, image: '../assets/images/earlAgustin/music-cover-01.png', title: "Dito Sa'kin", time: "5:30", listeners: "69,432,436", album: "Himig At Pag-ibig"},
+        { number: 5, image: '../assets/images/earlAgustin/music-cover-01.png', title: "Aya", time: "3:50", listeners: "141,523,634", album: "Himig At Pag-ibig"},
+    ],
+    'Rob Deniel': [
+        { number: 1, image: '../assets/images/robDeniel/music-cover-02.png', title: "Nandito Ako", time: "6:01", listeners: "345,538,723", album: "Single", artistProfile: "../assets/images/robDeniel/profile.jpg", artistCover: "../assets/images/robDeniel/profile.jpg", monthlyListeners: "6,439,172 Monthly Listeners"},
+        { number: 2, image: '../assets/images/robDeniel/music-cover-02.png', title: "Wala Na Bang Pag-ibig", time: "4:36", listeners: "912,827,798", album: "Single" },
+        { number: 3, image: '../assets/images/robDeniel/music-cover-03.png', title: "RomCom", time: "3:57", listeners: "436,512,643", album: "Wander Boy"},
+        { number: 4, image: '../assets/images/robDeniel/music-cover-03.png', title: " Miss Miss", time: "4:25", listeners: "436,512,643", album: "Wander Boy"},
+        { number: 5, image: '../assets/images/robDeniel/music-cover-01.png', title: "Simoy", time: "3:19", listeners: "79,567,645", album: "Single"},
+    ],
+    'Laufey': [
+        { number: 1, image: '../assets/images/laufey/music-cover-04.png', title: "From The Start", time: "2:49", listeners: "745,416,234", album: "Bewitched", artistProfile: "../assets/images/laufey/profile.jpg", artistCover: "../assets/images/laufey/profile.jpg", monthlyListeners: "24,296,271 Monthly Listeners"},
+        { number: 2, image: '../assets/images/laufey/music-cover-01.png', title: "Lover Girl", time: "2:44", listeners: "283,286,267", album: "A Matter of Time" },
+        { number: 3, image: '../assets/images/laufey/music-cover-04.png', title: "Promise", time: "3:54", listeners: "354,987,234", album: "Bewitched"},
+        { number: 4, image: '../assets/images/laufey/music-cover-03.png', title: "Falling Behind", time: "2:53", listeners: "423,561,198", album: "Everything I Know About Love"},
+        { number: 5, image: '../assets/images/laufey/music-cover-02.png', title: "Let You Break My Heart Again", time: "4:29", listeners: "243,521,432", album: "Single"},
+    ],
+    'Lana Del Rey': [
+        { number: 1, image: '../assets/images/lanaDelRey/music-cover-02.png', title: "Cinnamon Girl", time: "5:00", listeners: "243,864,245", album: "Norman Fucking Rockwell!", artistProfile: "../assets/images/lanaDelRey/profile.jpg", artistCover: "../assets/images/lanaDelRey/profile.jpg", monthlyListeners: "63,762,632 Monthly Listeners"},
+        { number: 2, image: '../assets/images/lanaDelRey/music-cover-05.png', title: "Young and Beautiful", time: "3:56", listeners: "745,531,924", album: "Single" },
+        { number: 3, image: '../assets/images/lanaDelRey/music-cover-03.png', title: "Summertime Sadness", time: "4:25", listeners: "768,563,123", album: "Born To Die"},
+        { number: 4, image: '../assets/images/lanaDelRey/music-cover-04.png', title: "Say Yes To Heaven", time: "3:29", listeners: "872,463,423", album: "Single"},
+        { number: 5, image: '../assets/images/lanaDelRey/music-cover-01.png', title: "Brooklyn Baby", time: "5:51", listeners: "423,687,543", album: "Ultraviolence"},
+    ],
+    'Maki': [
+        { number: 1, image: '../assets/images/maki/music-cover-01.png', title: "Kahel Na Langit", time: "3:36", listeners: "538,856,938", album: "KOLORCOASTER", artistProfile: "../assets/images/maki/profile.jpg", artistCover: "../assets/images/maki/profile.jpg", monthlyListeners: "4,401,125 Monthly Listeners"},
+        { number: 2, image: '../assets/images/maki/music-cover-01.png', title: "Dilaw", time: "3:12", listeners: "73,584,733", album: "KOLORCOASTER" },
+        { number: 3, image: '../assets/images/maki/music-cover-03.png', title: "Saan?", time: "3:26", listeners: "46,922,934", album: "Single"},
+        { number: 4, image: '../assets/images/maki/music-cover-02.png', title: "Kailan?", time: "3:54", listeners: "811,970,634", album: "Tanong"},
+        { number: 5, image: '../assets/images/maki/music-cover-01.png', title: "Namumula", time: "2:54", listeners: "673,548,234", album: "KOLORCOASTER"},
+    ],
+    'Rex Orange Country': [
+        { number: 1, image: '../assets/images/rexOrangeCountry/music-cover-01.png', title: "Love Me Not (feat. Rex Orange County)", time: "3:08", listeners: "227,128,785", album: "Single", artistProfile: "../assets/images/rexOrangeCountry/profile.jpg", artistCover: "../assets/images/rexOrangeCountry/profile.jpg", monthlyListeners: "18,756,440 Monthly Listeners"},
+        { number: 2, image: '../assets/images/rexOrangeCountry/music-cover-02.png', title: "Best Friend", time: "4:22", listeners: "847,187,931", album: "Single" },
+        { number: 3, image: '../assets/images/rexOrangeCountry/music-cover-03.png', title: "Television / So Far Good", time: "4:22", listeners: "485,004,434", album: "Apricot Princess"},
+        { number: 4, image: '../assets/images/rexOrangeCountry/music-cover-04.png', title: "Pluto Projector", time: "4:27", listeners: "628,295,105", album: "Single"},
+        { number: 5, image: '../assets/images/rexOrangeCountry/music-cover-03.png', title: "Happiness", time: "4:39", listeners: "365,891,231", album: "Apricot Princess"},
+    ],
+    'Daniel Caesar': [
+        { number: 1, image: '../assets/images/danielCaesar/music-cover-03.png', title: "Who Knows", time: "3:46", listeners: "1,534,628,791", album: "Speed Of Spergy", artistProfile: "../assets/images/danielCaesar/profile.jpg", artistCover: "../assets/images/danielCaesar/cover.jpg", monthlyListeners: "40,681,221 Monthly Listeners"},
+        { number: 2, image: '../assets/images/danielCaesar/music-cover-01.png', title: "Get You (feat. Kali Uchis)", time: "4:38", listeners: "903,172,214", album: "Freudian"},
+        { number: 3, image: '../assets/images/danielCaesar/music-cover-01.png', title: "Best Part (feat. H.E.R.)", time: "3:29", listeners: "851,270,395", album: "Freudian" },
+        { number: 4, image: '../assets/images/danielCaesar/music-cover-02.png', title: "Superpowers", time: "2:54", listeners: "621,098,243", album: "NEVER ENOUGH"},
+        { number: 5, image: '../assets/images/danielCaesar/music-cover-02.png', title: "Always", time: "3:45", listeners: "512,037,523", album: "NEVER ENOUGH"},
+    ],
+    'Frank Ocean': [
+        { number: 1, image: '../assets/images/frankOcean/music-cover-02.png', title: "Pink + White", time: "3:04", listeners: "1,850,345,434", album: "Blonde", artistProfile: "../assets/images/frankOcean/profile.jpg", artistCover: "../assets/images/frankOcean/profile.jpg", monthlyListeners: "40,047,513  Monthly Listeners"},
+        { number: 2, image: '../assets/images/frankOcean/music-cover-02.png', title: "Nights", time: "5:07", listeners: "176,458,485", album: "Blonde" },
+        { number: 3, image: '../assets/images/frankOcean/music-cover-02.png', title: "White Ferrari", time: "4:08", listeners: "695,712,875", album: "Blonde"},
+        { number: 4, image: '../assets/images/frankOcean/music-cover-01.png', title: "Thinkin Bout You", time: "3:40", listeners: "620,374,896,578", album: "channel ORANGE"},
+        { number: 5, image: '../assets/images/frankOcean/music-cover-01.png', title: "Lost", time: "3:54", listeners: "56,237,632,45", album: "channel ORANGE"},
+    ],
 };
 
 function showArtist(artistDisplayName) {
